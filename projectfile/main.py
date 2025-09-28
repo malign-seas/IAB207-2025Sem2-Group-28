28841e7dc266eeedde75f33118e3c972d3dc05ef
Wakenarey <rjaykitkat@gmail.com>
2025-09-28 17:23:47 +1000

    added helpful comments

--- projectfile/main.py
+++ projectfile/main.py
@@ -4,3 +4,11 @@ from website import create_app
 if __name__ == '__main__':
     app = create_app()
     app.run()
+
+# create database terminal commands
+# python3
+# >>> from website import db, create_app
+# >>> app = create_app()
+# >>> ctx = app.app_context()
+# >>> ctx.push()
+# >>> db.create_all()
--- projectfile/website/models.py
+++ projectfile/website/models.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False) #128 length for hashed passwords
 # class Event(db.Model):
 #     pass
 # class Comment(db.Model):
